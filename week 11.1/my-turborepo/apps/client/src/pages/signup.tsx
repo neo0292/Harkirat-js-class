31b1c174a0ef852aceb1b0dcf1ace1b3c674cc17
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-10-25 23:54:19 +0530

    nextjs project started

---
+++ week 11.1/my-turborepo/apps/client/src/pages/signup.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Signup  from 'ui/components/Signup'
+
+const signupPage = () => {
+  console.log('Signup from SignupPage',Signup);
+  return (
+    <div>hii
+      <Signup></Signup>
+    </div>
+  )
+}
+
+export default signupPage
